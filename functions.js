97d194a0e6a66d77c3dcfde1384b3dc180c7088a
Dan D'Andrea <DDandrea@infiniteenergy.com>
2016-07-11 10:18:22 -0400

    Add functions.js

---
+++ functions.js
@@ -0,0 +1,12 @@
+function getPlatformCoordinates(canvasWidth, canvasHeight, platformWidth, maxYCoordinateValue, numberOfPlatforms)
+{
+    var platforms = new Array();
+
+    var platform1 = {'x1': 50, 'y1': 50, 'x2': 80, 'y2': 50};
+    var platform1 = {'x1': 100, 'y1': 75, 'x2': 130, 'y2': 75};
+
+    platforms.push(platform1);
+    platforms.push(platform2);
+
+    return platforms;
+}
