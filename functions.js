1b07c07c95c1c3edb5265fc77218d01783bd9500
Dan D'Andrea <DDandrea@infiniteenergy.com>
2016-07-11 15:44:28 -0400

    Simplify line drawing

--- functions.js
+++ functions.js
@@ -2,11 +2,21 @@ function getPlatformCoordinates(canvasWidth, canvasHeight, platformWidth, maxYCo
 {
     var platforms = new Array();
 
-    var platform1 = {'x1': 50, 'y1': 50, 'x2': 80, 'y2': 50};
-    var platform2 = {'x1': 100, 'y1': 75, 'x2': 130, 'y2': 75};
+    var platform1 = {'x1': 350, 'y1': 300, 'x2': 450, 'y2': 300};
+    var platform2 = {'x1': 250, 'y1': 300, 'x2': 300, 'y2': 300};
 
     platforms.push(platform1);
     platforms.push(platform2);
 
     return platforms;
+}
+
+function drawLine(game, x1, y1, x2, y2, lineHeight)
+{
+        console.log("Drawing line from (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ")");
+
+        var line = game.add.graphics(0, 0);
+        line.lineStyle(lineHeight, 0xffffff, 1);
+        line.moveTo(x1, y1);
+        line.lineTo(x2, y2);
 }
